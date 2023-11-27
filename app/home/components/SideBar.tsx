51b777a04181a4f9f87dc8322b03a34f8f932c5e
Yernur <shalgymbayev.yernur2001@gmail.com>
2023-11-27 19:42:26 +0600

    implement hide create project button when no free chief editors

--- app/home/components/SideBar.tsx
+++ app/home/components/SideBar.tsx
@@ -2,33 +2,41 @@
 import { ProjectModel } from "@/app/model/ProjectModel";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { getAllProjects } from "../service/fetcher";
+import { getAllFreeChiefEditors, getAllProjects } from "../service/fetcher";
 import { ProjectsListView } from "./ProjectsListView";
 import Typography from "@mui/material/Typography/Typography";
 import { ProjectCreateButton } from "./ProjectCreateButton";
+import { ProfileModel } from "@/app/model/ProfileModel";
 
 export const SideBar = () => {
   const session = useSession();
   const [projectsList, setProjectsList] = useState<ProjectModel[]>([]);
   const [isFetching, setIsFetching] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [isCreatable, setIsCreatable] = useState(true);
   useEffect(() => {
     if (session.data?.user) {
       const {
         user: { idToken },
       } = session.data;
 
-      getAllProjects(idToken)
-        .then((data) => setProjectsList(data))
-        .catch(() => setIsError(true))
-        .finally(() => setIsFetching(false));
+      Promise.all([
+        getAllProjects(idToken)
+          .then((data) => setProjectsList(data))
+          .catch(() => setIsError(true)),
+        getAllFreeChiefEditors(idToken)
+          .then(
+            (data: ProfileModel[]) => data.length === 0 && setIsCreatable(false)
+          )
+          .catch(() => setIsError(true)),
+      ]).finally(() => setIsFetching(false));
     }
   }, [session]);
 
   const content = !(isFetching || isError) && (
     <>
       <ProjectsListView projectsList={projectsList} />
-      <ProjectCreateButton />
+      {isCreatable && <ProjectCreateButton />}
     </>
   );
   const loading = isFetching && <Typography>Loading...</Typography>;
